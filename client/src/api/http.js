60b941b9a8b1178a376662c1c672f215b9486be7
CodyBrada <codypredy@gmail.com>
2019-04-04 15:50:52 +0300

    Updated to use env url Small tweaks

--- client/src/api/http.js
+++ client/src/api/http.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
 export const http = axios.create({
-  baseURL: `${window.location.protocol}//${window.location.hostname}:${
-    process.env.REACT_APP_SERVER_PORT
-  }`
+  baseURL: process.env.REACT_APP_API_URL
 });
