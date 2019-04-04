2e9378065b167546aebf3c9560a897602df2eda4
CodyBrada <codypredy@gmail.com>
2019-04-04 15:00:06 +0300

    Updated Readme and some environment configuration

--- client/src/api/http.js
+++ client/src/api/http.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
 export const http = axios.create({
-  baseURL: `http://${window.location.hostname}:${process.env.REACT_APP_SERVER_PORT}`,
+  baseURL: `http://${window.location.hostname}:${
+    process.env.REACT_APP_SERVER_PORT
+  }`
 });
---
+++ server/Readme.md
@@ -0,0 +1,7 @@
+# NodeJS Server
+
+Basic NodeJS server using Express and nodemon.
+
+## Quick Start
+
+Run `npm start`.
